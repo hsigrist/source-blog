2eabf070bcb9df2f2c79303c43ad9125f5d7c84e
Hans Sigrist <hsigrist@gmail.com>
2017-04-16 01:49:33 -0300

    update evaluar

--- static/formacion_continua/para_que_evaluar/auto/para_que_evaluar.el
+++ static/formacion_continua/para_que_evaluar/auto/para_que_evaluar.el
@@ -5,12 +5,12 @@
                      '(("memoir" "12pt" "letterpaper" "article" "x11names")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("minitoc" "tight" "undotted" "nohints" "spanish") ("csquotes" "style=spanish") ("biblatex" "hyperref=false" "backend=biber" "style=apa" "citetracker=true" "pagetracker=true") ("geometry" "left=2cm" "right=2cm" "top=2cm" "bottom=3cm" "paperheight=33cm")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -40,36 +40,37 @@
     "longtable"
     "geometry")
    (LaTeX-add-labels
-    "sec:orgf8af30c"
-    "sec:orgaabafbf"
-    "sec:orgbaa5c6d"
-    "sec:orgf891546"
-    "sec:orgefc78c2"
-    "sec:org670c75a"
-    "sec:orgc80d2ed"
-    "sec:org38ea603"
-    "sec:org3017700"
-    "sec:org32a676f"
-    "sec:org42cb2b9"
-    "sec:org68cccfb"
-    "sec:org4d30c99"
-    "sec:org228339a"
-    "sec:orgb2d7d27"
-    "sec:org9c3ebbc"
-    "sec:org42a4d6e"
-    "sec:org94f1fbf"
-    "sec:org4c68bae"
-    "sec:org4a0daeb"
-    "sec:orgdaaeafa"
-    "sec:org8695847"
-    "sec:org6cb52d6"
-    "sec:org05b0b6a"
-    "sec:org919265d"
-    "sec:orga4d4c5a"
-    "sec:org551a5ae"
-    "sec:orgc403797"
-    "sec:orgdd1ab34"
-    "sec:orga442fb9")
+    "sec:org167c68d"
+    "sec:org28d2896"
+    "sec:org912954d"
+    "sec:org7f57519"
+    "sec:orgbcaa1b4"
+    "sec:orgf8cedb7"
+    "sec:org2d1f986"
+    "sec:orgb5a8eb2"
+    "sec:org185324e"
+    "sec:orgf15f7e8"
+    "sec:orgd42c451"
+    "sec:orgb2606ed"
+    "sec:org62740da"
+    "sec:org9541274"
+    "sec:org1f6aab8"
+    "sec:orge24c6e2"
+    "sec:orgcb7d1f0"
+    "sec:orgf265531"
+    "sec:org516a715"
+    "sec:orgb9fecef"
+    "sec:orge711ae3"
+    "sec:org29e5572"
+    "sec:org5e7f0c8"
+    "sec:orgf650e07"
+    "sec:orgc33b992"
+    "sec:org3d36a55"
+    "sec:org71bc31b"
+    "sec:orgdd10746"
+    "sec:orgcb71e16"
+    "sec:org02eef9f"
+    "sec:org0101e59")
    (LaTeX-add-environments
     "teorema"
     "lema"
